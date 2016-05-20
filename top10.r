16f3d953ad4fe87277bfdc27e2270a3ea9712980
Sam Jacob <Sam.Jacob@synechron.com>
2016-05-20 22:19:11 +0530

    Top 10 reports -v2

--- top10.r
+++ top10.r
@@ -3,4 +3,4 @@ library(sqldf)
 
 
 thriller <- sqldf("select movieId,Title from mov2 where Thriller = 1")
-top_thrill <- sqldf("select Title, avg(ratings) from ")
+top_thrill <- sqldf("select Title, avg(Rating) from ratings join thriller using(movieId) group by MovieId")
