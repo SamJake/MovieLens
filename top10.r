d0ec68b9413745ac0082a8ba96f6f2a4743cee1b
Sam Jacob <Sam.Jacob@synechron.com>
2016-05-23 16:48:58 +0530

    Top 10 reports for any category | Functionized -v5

--- top10.r
+++ top10.r
@@ -7,9 +7,22 @@ library(sqldf)
 
 
 
-category <- "Romance"
-sql <- sprintf("select movieId, Title from mov2 where %s = 1",category)
+category <- "Action"
 
-movies_by_category <- sqldf(sql)
-top10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) desc limit 10")
-worst10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) limit 10")
+top10 <- function(category)
+{
+  sql <- sprintf("select movieId, Title from mov2 where %s = 1",category)
+  movies_by_category <- sqldf(sql)  
+  top10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) desc limit 10")
+}
+
+
+worst10 <- function(category)
+{
+  sql <- sprintf("select movieId, Title from mov2 where %s = 1",category)
+  movies_by_category <- sqldf(sql)  
+  worst10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) limit 10")
+}
+
+top10(category)
+worst10(category)
