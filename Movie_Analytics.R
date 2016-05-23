dbb31bd4bcdfc9f4e1c3cd3ad1dbbe62b5f0a37e
Sam Jacob <Sam.Jacob@synechron.com>
2016-05-23 19:44:42 +0530

    Movie Analytics -v1

---
+++ Movie_Analytics.R
@@ -0,0 +1,10 @@
+install.packages("sqldf")
+library(sqldf)
+
+id <- 1721 #Titanic
+sql <- sprintf("select UserId, Rating from ratings where movieId = '%s'",id)
+all_users <- sqldf(sql)
+
+all_users_upd <- sqldf("select userid, AgeRange, Gender, Rating from all_users join users_updated using(userid)")
+
+analytics <- sqldf("select AgeRange, Gender, count(Rating), avg(Rating) as Average from all_users_upd group by AgeRange, Gender order by AgeRange, Gender")
