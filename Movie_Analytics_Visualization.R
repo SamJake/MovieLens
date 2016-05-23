90e0d4f1a43579e64dd7e5eef8dd2365bc238a7a
Sam Jacob <Sam.Jacob@synechron.com>
2016-05-23 22:52:32 +0530

    Movie Analytics | Report -v2

--- Movie_Analytics_Visualization.R
+++ Movie_Analytics_Visualization.R
@@ -1,6 +1,34 @@
 install.packages("ggplot2")
 library(ggplot2)
 
+plot_analytics <- function(df,Title)
+{
+g <- ggplot(df,aes(x=factor(df$AgeRange),y=df$Average)) + geom_bar(position="dodge",stat="identity",aes(fill=factor(df$Gender)))
+g <- g + xlab("") + ylab("Ratings") + ggtitle(Title)
+g <- g + coord_cartesian(ylim = c(3,5)) + scale_fill_discrete(name="Gender")
+}
 
 df <- movie_analytics(1240)
-g <- ggplot(df,aes(x=factor(df$AgeRange),y=df$Average)) + geom_bar(position="dodge",stat="identity",aes(fill=factor(df$AgeRange)))
+g <- plot_analytics(df,"Terminator")
+g
+
+df <- movie_analytics(1)
+g <- plot_analytics(df,"Toy Story")
+g
+
+df <- movie_analytics(47)
+g <- plot_analytics(df,"Seven")
+g
+
+df <- movie_analytics(261)
+g <- plot_analytics(df,"Little Women")
+g
+
+
+df <- movie_analytics(296)
+g <- plot_analytics(df,"Pulp Fiction")
+g
+
+df <- movie_analytics(364)
+g <- plot_analytics(df,"The Lion King")
+g
