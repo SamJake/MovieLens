e4e61820ab0d2ff7c86bf70923842140304717f8
Sam Jacob <Sam.Jacob@synechron.com>
2016-05-23 16:43:18 +0530

    Top 10 reports for any category -v4

--- top10.r
+++ top10.r
@@ -2,5 +2,14 @@ install.packages("sqldf")
 library(sqldf)
 
 
-thriller <- sqldf("select movieId,Title from mov2 where Thriller = 1")
-top_thrill <- sqldf("select Title, avg(Rating) from ratings join thriller using(movieId) group by MovieId order by avg(Rating)")
+#thriller <- sqldf("select movieId,Title from mov2 where Thriller = 1")
+#top_thrill <- sqldf("select Title, avg(Rating) from ratings join thriller using(movieId) group by MovieId order by avg(Rating)")
+
+
+
+category <- "Romance"
+sql <- sprintf("select movieId, Title from mov2 where %s = 1",category)
+
+movies_by_category <- sqldf(sql)
+top10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) desc limit 10")
+worst10_movies_by_category <- sqldf("select Title, avg(Rating) from ratings join movies_by_category using(movieId) group by MovieId order by avg(Rating) limit 10")
